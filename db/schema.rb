1f794507e0cd2df66a5daecd0ddc38cf4bc63508
j094097 <j094097@gmail.com>
2025-10-30 04:00:16 +0800

    test pg

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,14 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[8.0].define(version: 2025_10_29_180251) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_catalog.plpgsql"
+
   create_table "estates", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "role_id"
     t.float "value"
-    t.datetime "custom_timestamp"
+    t.datetime "custom_timestamp", precision: nil
   end
 end
